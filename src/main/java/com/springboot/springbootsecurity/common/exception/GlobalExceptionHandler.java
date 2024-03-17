8cb16ac55d4b79d5aeca82842cd0bf512517fb35
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-03-17 15:03:24 +0300

    Task 6 : Define GlobalExceptionHandler

---
+++ src/main/java/com/springboot/springbootsecurity/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,63 @@
+package com.springboot.springbootsecurity.common.exception;
+
+import com.springboot.springbootsecurity.common.model.CustomError;
+import jakarta.validation.ConstraintViolationException;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(final MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach(
+                error -> {
+                    String fieldName = ((FieldError) error).getField();
+                    String message = error.getDefaultMessage();
+                    errors.put(fieldName, message);
+                }
+        );
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    protected ResponseEntity<Object> handlePathVariableErrors(final ConstraintViolationException constraintViolationException) {
+
+        final List<CustomError.CustomSubError> subErrors = new ArrayList<>();
+        constraintViolationException.getConstraintViolations()
+                .forEach(constraintViolation ->
+                        subErrors.add(
+                                CustomError.CustomSubError.builder()
+                                        .message(constraintViolation.getMessage())
+                                        .field(StringUtils.substringAfterLast(constraintViolation.getPropertyPath().toString(), "."))
+                                        .value(constraintViolation.getInvalidValue() != null ? constraintViolation.getInvalidValue().toString() : null)
+                                        .type(constraintViolation.getInvalidValue().getClass().getSimpleName())
+                                        .build()
+                        )
+                );
+
+        return new ResponseEntity<>(subErrors, HttpStatus.BAD_REQUEST);
+
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    protected ResponseEntity<?> handleRuntimeException(final RuntimeException runtimeException) {
+        return new ResponseEntity<>(runtimeException, HttpStatus.NOT_FOUND);
+    }
+
+}
