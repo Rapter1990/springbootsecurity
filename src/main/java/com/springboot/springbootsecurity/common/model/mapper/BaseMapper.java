4a5fb400f979ea28eb67234235bf4ca4bee7165f
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-03-17 14:58:12 +0300

    Task 4 : Define BaseMapper in common package

---
+++ src/main/java/com/springboot/springbootsecurity/common/model/mapper/BaseMapper.java
@@ -0,0 +1,12 @@
+package com.springboot.springbootsecurity.common.model.mapper;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface BaseMapper<S, T> {
+
+    T map(S source);
+
+    List<T> map(Collection<S> sources);
+
+}
