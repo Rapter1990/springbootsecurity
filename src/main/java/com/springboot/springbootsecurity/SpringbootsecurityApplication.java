d48a601bf6fe8a5c5cdb013a63ec866712b0d3d0
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-03-17 14:26:50 +0300

    Task 1 : Initial commit

---
+++ src/main/java/com/springboot/springbootsecurity/SpringbootsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootsecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/springbootsecurity/SpringbootsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
