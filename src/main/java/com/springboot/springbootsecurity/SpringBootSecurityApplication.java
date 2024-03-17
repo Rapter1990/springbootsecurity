da89f86269cb04131720a71516cb21b3e4cc0b54
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-03-17 16:51:08 +0300

    Task 8 : Revise class names

--- src/main/java/com/springboot/springbootsecurity/SpringBootSecurityApplication.java
+++ src/main/java/com/springboot/springbootsecurity/SpringBootSecurityApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootsecurityApplication {
+public class SpringBootSecurityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootsecurityApplication.class, args);
+		SpringApplication.run(SpringBootSecurityApplication.class, args);
 	}
 
 }
