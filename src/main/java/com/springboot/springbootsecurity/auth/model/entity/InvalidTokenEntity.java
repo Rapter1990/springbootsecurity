e2a8b44f1544c008a7f7750fedf87a600a3a264d
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-03-18 23:12:23 +0300

    Task 23 : Define InvalidTokenEntity

---
+++ src/main/java/com/springboot/springbootsecurity/auth/model/entity/InvalidTokenEntity.java
@@ -0,0 +1,26 @@
+package com.springboot.springbootsecurity.auth.model.entity;
+
+import com.springboot.springbootsecurity.common.model.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Table(name = "INVALID_TOKEN")
+public class InvalidTokenEntity extends BaseEntity {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "TOKEN_ID")
+    private String tokenId;
+
+}
