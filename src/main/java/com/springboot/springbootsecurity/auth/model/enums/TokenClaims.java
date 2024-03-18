6ba5e2ce12deebe7c70846acd7466aa40a3caf17
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-03-18 23:13:09 +0300

    Task 24 : Define TokenClaims for JWT Token's feature

---
+++ src/main/java/com/springboot/springbootsecurity/auth/model/enums/TokenClaims.java
@@ -0,0 +1,25 @@
+package com.springboot.springbootsecurity.auth.model.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum TokenClaims {
+
+    USER_ID("userId"),
+    USER_TYPE("userType"),
+    USER_STATUS("userStatus"),
+    USER_FIRST_NAME("userFirstName"),
+    USER_LAST_NAME("userLastName"),
+    USER_EMAIL("userEmail"),
+    USER_PHONE_NUMBER("userPhoneNumber"),
+    STORE_TITLE("storeTitle"),
+    ISSUED_AT("iat"),
+    EXPIRES_AT("exp"),
+    ALGORITHM("alg"),
+    TYP("typ");
+
+    private final String value;
+
+}
