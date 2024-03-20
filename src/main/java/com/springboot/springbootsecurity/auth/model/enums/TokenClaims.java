c45cfc62588bd5fd8a86f1edb3a68eb01941f112
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-03-20 23:30:14 +0300

    Task 48 : Add JWT_ID to TokenClaims

--- src/main/java/com/springboot/springbootsecurity/auth/model/enums/TokenClaims.java
+++ src/main/java/com/springboot/springbootsecurity/auth/model/enums/TokenClaims.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum TokenClaims {
 
+    JWT_ID("jti"),
     USER_ID("userId"),
     USER_TYPE("userType"),
     USER_STATUS("userStatus"),
