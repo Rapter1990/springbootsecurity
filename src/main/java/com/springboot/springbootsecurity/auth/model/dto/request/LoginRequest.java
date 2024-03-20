90babe120a20e9edb6d7486830eca8cc3b7b837f
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-03-20 18:24:08 +0300

    Task 34 : Revise LoginRequest

--- src/main/java/com/springboot/springbootsecurity/auth/model/dto/request/LoginRequest.java
+++ src/main/java/com/springboot/springbootsecurity/auth/model/dto/request/LoginRequest.java
@@ -1,11 +1,13 @@
 package com.springboot.springbootsecurity.auth.model.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class LoginRequest {
 
     @NotBlank
