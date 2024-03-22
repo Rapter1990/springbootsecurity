1d8e115f79d667adc57257da4222787c04953513
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-03-22 19:10:07 +0300

    Task 58 : Define UserEntity and User Domain Object

--- src/main/java/com/springboot/springbootsecurity/user/model/entity/UserEntity.java
+++ src/main/java/com/springboot/springbootsecurity/user/model/entity/UserEntity.java
@@ -1,4 +1,71 @@
 package com.springboot.springbootsecurity.user.model.entity;
 
-public class UserEntity {
+import com.springboot.springbootsecurity.auth.model.enums.TokenClaims;
+import com.springboot.springbootsecurity.auth.model.enums.UserStatus;
+import com.springboot.springbootsecurity.auth.model.enums.UserType;
+import com.springboot.springbootsecurity.common.model.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@Setter
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "CASE_USER")
+public class UserEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "ID")
+    private String id;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(
+            name = "PHONE_NUMBER",
+            length = 20
+    )
+    private String phoneNumber;
+
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    private UserType userType = UserType.USER;
+
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    private UserStatus userStatus = UserStatus.ACTIVE;
+
+    public Map<String, Object> getClaims() {
+
+        final Map<String, Object> claims = new HashMap<>();
+
+        claims.put(TokenClaims.USER_ID.getValue(), this.id);
+        claims.put(TokenClaims.USER_TYPE.getValue(), this.userType);
+        claims.put(TokenClaims.USER_STATUS.getValue(), this.userStatus);
+        claims.put(TokenClaims.USER_FIRST_NAME.getValue(), this.firstName);
+        claims.put(TokenClaims.USER_LAST_NAME.getValue(), this.lastName);
+        claims.put(TokenClaims.USER_EMAIL.getValue(), this.email);
+        claims.put(TokenClaims.USER_PHONE_NUMBER.getValue(), this.phoneNumber);
+
+        return claims;
+
+    }
+
 }
