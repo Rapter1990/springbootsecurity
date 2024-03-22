79303d9be49d85a97b1ff9dd0a3b876f4c770ca2
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-03-22 19:14:45 +0300

    Task 62 : Define UserRepository

---
+++ src/main/java/com/springboot/springbootsecurity/user/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.springboot.springbootsecurity.user.repository;
+
+import com.springboot.springbootsecurity.user.model.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, String> {
+
+    boolean existsUserEntityByEmail(final String email);
+
+    Optional<UserEntity> findUserEntityByEmail(final String email);
+
+}
