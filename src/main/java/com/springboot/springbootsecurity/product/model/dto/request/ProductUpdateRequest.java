09828d37084c7adf661ba495d921b63b77beff52
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-03-24 16:01:22 +0300

    Task 82 : Define ProductCreateRequest and ProductUpdateRequest

---
+++ src/main/java/com/springboot/springbootsecurity/product/model/dto/request/ProductUpdateRequest.java
@@ -0,0 +1,34 @@
+package com.springboot.springbootsecurity.product.model.dto.request;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductUpdateRequest {
+
+    @Size(
+            min = 1,
+            message = "Product name can't be blank."
+    )
+    private String name;
+
+    @DecimalMin(
+            value = "0.0001",
+            message = "Amount must be bigger than 0"
+    )
+    private BigDecimal amount;
+
+    @DecimalMin(
+            value = "0.0001",
+            message = "Unit Price must be bigger than 0"
+    )
+    private BigDecimal unitPrice;
+
+}
