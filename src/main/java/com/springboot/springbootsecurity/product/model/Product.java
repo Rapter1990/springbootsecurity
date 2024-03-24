a9a44d2ba0a1a1269703f080c78df88f90b39ad0
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-03-24 15:42:35 +0300

    Task 78 : Define Product Domain Object

---
+++ src/main/java/com/springboot/springbootsecurity/product/model/Product.java
@@ -0,0 +1,26 @@
+package com.springboot.springbootsecurity.product.model;
+
+import com.springboot.springbootsecurity.common.model.BaseDomainModel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product extends BaseDomainModel {
+
+    private String id;
+    private String name;
+    private BigDecimal amount;
+    private BigDecimal unitPrice;
+
+}
