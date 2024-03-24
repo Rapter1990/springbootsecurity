3f68230292113a8a66f7fa504f6ece0cfa0b6ef1
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-03-24 16:05:21 +0300

    Task 84 : Define ProductPagingRequest

---
+++ src/main/java/com/springboot/springbootsecurity/product/model/dto/request/ProductPagingRequest.java
@@ -0,0 +1,14 @@
+package com.springboot.springbootsecurity.product.model.dto.request;
+
+import com.springboot.springbootsecurity.common.model.dto.request.CustomPagingRequest;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+public class ProductPagingRequest extends CustomPagingRequest {
+
+
+}
